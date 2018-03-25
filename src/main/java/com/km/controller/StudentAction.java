eef70ef5799ae4b36db258bb6fc9ce4590c9003a
jiangxu017 <Arvin_Key@163.com>
2018-03-25 13:18:49 +0800

    test

--- src/main/java/com/km/controller/StudentAction.java
+++ src/main/java/com/km/controller/StudentAction.java
@@ -1,13 +1,10 @@
 package com.km.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.alibaba.fastjson.JSON;
 import com.km.model.User;
 import com.km.redis.RedisUtil;
 import com.km.service.UserService;
